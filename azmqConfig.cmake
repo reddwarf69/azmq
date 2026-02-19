c768c2e96110e84d91c94476279bba4c8fcaa6c1
Cristian Morales Vega <christian.morales.vega@gmail.com>
2026-02-19 14:49:20 +0000

    CMake: Simplify Boost::system search

--- azmqConfig.cmake
+++ azmqConfig.cmake
@@ -1,6 +1,5 @@
 include(CMakeFindDependencyMacro)
-find_package(Boost 1.68 COMPONENTS date_time thread chrono random REQUIRED)
-find_package(Boost 1.68 COMPONENTS system)
+find_package(Boost 1.68 COMPONENTS date_time thread chrono random OPTIONAL_COMPONENTS system REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindAzmqLibzmq.cmake)
 find_package(Threads REQUIRED)
