27847840a2d1ad00f673598f5b29f03ced9d2857
Cristian Morales Vega <christian.morales.vega@gmail.com>
2026-02-19 14:49:20 +0000

    azmqConfig: Replace find_package with find_dependency
    It's the correct way, and I guess the reason
    include(CMakeFindDependencyMacro) was added in the first place.

--- azmqConfig.cmake
+++ azmqConfig.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
-find_package(Boost 1.68 OPTIONAL_COMPONENTS system REQUIRED)
+find_dependency(Boost 1.68 OPTIONAL_COMPONENTS system)
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindAzmqLibzmq.cmake)
-find_package(Threads REQUIRED)
+find_dependency(Threads)
 include("${CMAKE_CURRENT_LIST_DIR}/azmqTargets.cmake")
