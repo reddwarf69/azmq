1b93eb447c9bcb2ee33074c0b54afd5250977a09
Cristian Morales Vega <christian.morales.vega@gmail.com>
2026-02-20 11:21:12 +0000

    azmqConfig: Stop setting CMAKE_THREAD_PREFER_PTHREAD
    That was a Irix-specific variable removed in CMake 3.14. The original
    intention was likely to set THREADS_PREFER_PTHREAD_FLAG, but the user
    should be able to decide.

--- azmqConfig.cmake
+++ azmqConfig.cmake
@@ -3,6 +3,5 @@ find_package(Boost 1.68 COMPONENTS date_time thread chrono random REQUIRED)
 find_package(Boost 1.68 COMPONENTS system)
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindAzmqLibzmq.cmake)
-set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 find_package(Threads REQUIRED)
 include("${CMAKE_CURRENT_LIST_DIR}/azmqTargets.cmake")
